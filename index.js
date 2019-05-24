0e363bef04dea36b6696ed9de39693efba1ec71d
David Strauch <dmaster18@gmail.com>
2019-05-24 16:42:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ function inc(n) {
   return n++
 }
 
-it('inc(n) increments n and returns the result', function() {
-  expect(inc(a)).toEqual(a + 1)
-})
+
 
 it('dec(n) decrements n and returns the result', function() {
   expect(dec(a)).toEqual(a - 1)
