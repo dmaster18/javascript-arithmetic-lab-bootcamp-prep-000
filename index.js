c74dff8e6d68dac69c7d7516e8d131572a360d8b
David Strauch <dmaster18@gmail.com>
2019-05-24 16:52:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var a, b
+function add(a,b)
+{return a+b}
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
 
 it('add(a, b) adds two numbers and returns the result', function() {
   expect(add(a, b)).toEqual(a + b)
